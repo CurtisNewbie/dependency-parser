81796bc2491ded177c1ce41f1c5e8333f2272b6d
yongj.zhuang <yongj.zhuang@outlook.com>
2024-02-28 12:24:15 +0800

    Modified:  gen.sh

--- gen.sh
+++ gen.sh
@@ -1,2 +1,3 @@
 source ~/.bashrc
-python3 $GIT_PATH/dependency-parser/mod_parser.py $GIT_PATH > test.txt && gen_graph test.txt && rm test.txt && open out.svg
+out="$GIT_PATH/dependency-parser/out.svg"
+python3 $GIT_PATH/dependency-parser/mod_parser.py $GIT_PATH > test.txt && dot -Tsvg test.txt > "$out" && rm test.txt && open "$out"
